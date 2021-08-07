2765a77a1fc00b566ecbdb733def7ff6f12f636f
Mykola Gnatiuk <gmstudio1916@gmail.com>
2021-08-07 10:21:31 +0300

    [FIX] JS form logic refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 /*!
-* Start Bootstrap - Scrolling Nav v5.0.2 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Scrolling Nav v5.0.2 (https://startbootstrap.com/template/scrolling-nav)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+ */
 //
 // Scripts
 // 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -23,7 +22,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItems.map(function(responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
@@ -33,35 +32,54 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-function sendForm(event)
-{
-    let name = getValueById("name");
-    let from = getValueById("from");
-    let subject = getValueById("subject");
-    let text = getValueById("text");
+function sendForm(event) {
 
-    if(getValueById("name")){
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://mgnatiuk-functions.azurewebsites.net/api/ReciveEmailFunction?code=QzejLcV6D07GLg1puWfOuapleHrPYJ6Nea4aDXM8atPVC1eDXJP8Xw==&from="+from+"&subject="+subject+"&text="+text+"&name=" + name, true);
-        xhr.send();
-
-        resetValueFor("name");
-        resetValueFor("from");
-        resetValueFor("subject");
-        resetValueFor("text");
-    
-        swal("Success", "Email was sent", "success");
-    }else{
-        swal("Warning", "Email is required", "warning");
+    var formData = {
+        name: getValueById("name"),
+        from: getValueById("from"),
+        subject: getValueById("subject"),
+        text: getValueById("text")
     }
+
+    if (!formData.name && !formData.from && !formData.subject && !formData.text) {
+        swal("Warning", "Some fields of this form are empty.", "warning");
+        event.preventDefault();
+    }
+
+    if (!validateEmail(formData.from)) {
+        swal("Warning", "Email is not valid.", "warning");
+        event.preventDefault();
+    }
+
+    sendEmail(formData);
 }
 
- function resetValueFor(id) {
+function sendEmail(formData) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://mgnatiuk-functions.azurewebsites.net/api/ReciveEmailFunction?code=QzejLcV6D07GLg1puWfOuapleHrPYJ6Nea4aDXM8atPVC1eDXJP8Xw==&from=" + formData.from + "&subject=" + formData.subject + "&text=" + formData.text + "&name=" + formData.name, true);
+    xhr.send();
+    resetValues();
+    swal("Success", "Email was sent", "success");
+}
+
+function resetValues() {
+    resetValueFor("name");
+    resetValueFor("from");
+    resetValueFor("subject");
+    resetValueFor("text");
+}
+
+function resetValueFor(id) {
     var element = document.getElementById(id);
     element.value = "";
-  }   
-  
-  function getValueById(id){
-      var value = document.getElementById(id).value;
-      return value;
-  }
+}
+
+function getValueById(id) {
+    var value = document.getElementById(id).value;
+    return value;
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
