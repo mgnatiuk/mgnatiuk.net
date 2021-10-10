ac461a3b61f5d4336f459177a80ecd5b65d2a4c5
Mykola Gnatiuk <gmstudio1916@gmail.com>
2021-10-10 11:16:45 +0200

    [FEAT] Text animations.

--- js/scripts.js
+++ js/scripts.js
@@ -79,3 +79,47 @@ function resetValues(fieldid) {
         fields[index].value = '';
     }
 }
+
+(function ($) {
+    $(document).ready(function () {
+        $.jScrollability([
+            {
+                'selector': '.text-fade-animation',
+                'start': function ($el) { return $el.offset().top + $el.height() },
+                'end': 'parent',
+                'fn': function ($el, pcnt) {
+                    var $spans = $el.find('span');
+                    var point = Math.floor(($spans.length + 1) * pcnt);
+                    $spans.each(function (i, el) {
+                        var $span = $(el);
+                        if (i < point) {
+                            $span.addClass('visible');
+                        } else {
+                            $span.removeClass('visible');
+                        }
+                    });
+                }
+            }
+        ]);
+
+        $.jScrollability([
+            {
+                'selector': '.text-fade-animation',
+                'start': function ($el) { return $el.offset().top + $el.height() },
+                'end': 'parent',
+                'fn': function ($el, pcnt) {
+                    var $li = $el.find('li');
+                    var point = Math.floor(($li.length + 1) * pcnt);
+                    $li.each(function (i, el) {
+                        var $span = $(el);
+                        if (i < point) {
+                            $span.addClass('visible');
+                        } else {
+                            $span.removeClass('visible');
+                        }
+                    });
+                }
+            }
+        ]);
+    });
+})(jQuery);
