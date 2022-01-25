2ecae05b754cd654b0c2010cfdf9c5e09f3c134b
Mykola Gnatiuk <gmstudio1916@gmail.com>
2022-01-25 14:39:18 +0100

    [FEAT] Config file security.

--- api/ContactFormHttpFunction/ContactFormHttpFunction.cs
+++ api/ContactFormHttpFunction/ContactFormHttpFunction.cs
@@ -23,7 +23,7 @@ namespace ContactFormHttpFunction
             MessageDto message = JsonConvert.DeserializeObject<MessageDto>(requestBody);
             message.Id = Guid.NewGuid();
 
-            string body = $"👤 NEW MESSAGE FROM\n{message.Name} ({message.Email})\n\n📍 SUBJECT: \n{message.Subject}\n\n📧 MESSAGE:\n\n{message.Message}";
+            string body = $"👤 NEW MESSAGE FROM\n{message.Name} ({message.Email})\n\n📍 SUBJECT: \n{message.Subject}\n\n📧 MESSAGE:\n{message.Message}";
 
             var botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN"));
 
